6696fb5fc924ec4b7b04a0915bcad3b77de97df2
Vittorio-Carbone <113420748+Vittorio-Carbone@users.noreply.github.com>
2024-03-27 09:01:32 +0100

    trascrizione Fonetica prima della consegna alla logopedista

--- Progetto Logopedista/progetto_esame/src/app/home/home.page.ts
+++ Progetto Logopedista/progetto_esame/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { startWith } from 'rxjs';
 
 
 @Component({
@@ -33,8 +34,10 @@ export class HomePage implements OnInit {
   // fonemi = ['a', 'i', 'j', 'e', 'o', 'u', 'w', 'p', 'b', 'm', 'n', 't', 'd', 'g', 'k', 'ɲ:', 'k', 'r', 'l', 'f', 'v', 's', 'z', 'ʣ', 'ʦ:', 'ʎ:i', 'ʎ:i', 'ʎ:o', 'ʎ:u', 'ʎ:e', 'ʃe', 'ʃi', 'ʃo', 'ʃa', 'ʃu', 'ʧa', 'ʧo', 'ʧe', 'ʧi', 'ʧu', 'ʤa', 'ʤo', 'ʤe', 'ʤi', 'ʤu', '', 'ʤ', 'i', ':'];
   // scritta = ["glia", "glio", "gliu", "glie", 'scio', 'scia', 'sciu', "gli", 'sce', 'sci', 'cia', 'cio', 'ciu', 'gia', 'gio', "giu", "gn", "ce", "ci", "ge", "gi", 'a', 'i', 'i', 'e', 'o', 'u', 'u', 'p', 'b', 'm', 'n', 't', 'd', 'g', 'c', 'q', 'r', 'l', 'f', 'v', 's', 's', 'z', 'z', 'h', 'j', 'y' ]
   // fonemi = ['ʎ:a', 'ʎ:o', 'ʎ:u', 'ʎ:e', 'ʃo', 'ʃa', 'ʃu', 'ʎ:i','ʃe', 'ʃi', 'ʧa', 'ʧo', 'ʧu', 'ʤa', 'ʤo', 'ʤu', 'ɲ:', 'ʧe',  'ʧi',  'ʤe', 'ʤi', 'a', 'i', 'j', 'e', 'o', 'u', 'w', 'p', 'b', 'm', 'n', 't', 'd', 'g', 'k','k', 'r', 'l', 'f', 'v', 's', 'z', 'ʣ', 'ʦ:', '', 'ʤ', 'i' ]
-  scritta = ["glia", "glio", "gliu", "glie", 'scio', 'scia', 'sciu', "gli", 'sce', 'sci', 'cia', 'cio', 'ciu', 'gia', 'gio', "giu", "gn", "ce", "ci", "ge", "gi", 'j', 'a', 'e', 'o',  'p', 'b', 'm', 'n', 't', 'd', 'q', 'r', 'l', 'f', 'v', 'z', 'z', 'h', 'y','*' ]
-  fonemi = ['ʎ:à', 'ʎ:ò', 'ʎ:ù', 'ʎ:è', 'ʃò', 'ʃà', 'ʃù', 'ʎ:ì','ʃè', 'ʃì', 'ʧà', 'ʧò', 'ʧù', 'ʤà', 'ʤò', 'ʤù', 'ɲ:', 'ʧè',  'ʧì',  'ʤè', 'ʤì', 'ʤ', 'a', 'e', 'o', 'p', 'b', 'm', 'n', 't', 'd', 'k', 'r', 'l', 'f', 'v', 'ʣ', 'ʦ:', '', 'i','j' ]
+  scritta = ["glia", "glio", "gliu", "glie", 'scio', 'scia', 'sciu', "gli", 'sce', 'sci', 'cia', 'cio', 'ciu', 'gia', 'gio', "giu", "gn", "ce", "ci", "ge", "gi", 'j', 'a', 'e', 'o', 'p', 'b', 'm', 'n', 't', 'd', 'q', 'r', 'l', 'f', 'v']
+  fonemi = ['ʎ:à', 'ʎ:ò', 'ʎ:ù', 'ʎ:è', 'ʃò', 'ʃà', 'ʃù', 'ʎ:ì', 'ʃè', 'ʃì', 'ʧà', 'ʧò', 'ʧù', 'ʤà', 'ʤò', 'ʤù', 'ɲ:', 'ʧè', 'ʧì', 'ʤè', 'ʤì', 'ʤ', 'a', 'e', 'o', 'p', 'b', 'm', 'n', 't', 'd', 'k', 'r', 'l', 'f', 'v']
+  scrittaDopo = ['y', '*', 'h']
+  fonemiDopo = ['i', 'j', '']
   registra() {
     this.micON = !this.micON;
     if (this.micON) {
@@ -84,61 +87,146 @@ export class HomePage implements OnInit {
   }
 
   trascrizioneFonemi() {
-    let daTrascrivere :any = this.trascritto;
+    this.trascritto = this.trascritto.toLowerCase();
+    // let daTrascrivere: any = this.trascritto.split(" ");
+    let daTrascrivere: any = this.trascritto;
+    console.log(daTrascrivere);
     this.trascritto = "";
     for (let i = 0; i < this.scritta.length; i++) {
-      //Controllo delle i
-      if(daTrascrivere.includes("ia") || daTrascrivere.includes("ie") || daTrascrivere.includes("io") || daTrascrivere.includes("iu")){
-        daTrascrivere=daTrascrivere.replaceAll("i","*");
+      if (daTrascrivere.includes(this.scritta[i])) {
+        daTrascrivere = daTrascrivere.replaceAll(this.scritta[i], this.fonemi[i]);
       }
-      else
-      {
-        daTrascrivere=daTrascrivere.replaceAll("i","i");
+    }
+    let daTrascrivere2: any = daTrascrivere.split(" ");
+    console.log(daTrascrivere2)
+    for (let daTra of daTrascrivere2) {
+      // for (let i = 0; i < this.scritta.length; i++) {
+      // Controllo delle I
+      if (daTra.includes("ia") || daTra.includes("ie") || daTra.includes("io") || daTra.includes("iu")) {
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ia", "ja"));
+        daTra = daTra.replaceAll("ia", "ja")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ie", "je"));
+        daTra = daTra.replaceAll("ie", "je")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("io", "jo"));
+        daTra = daTra.replaceAll("io", "jo")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("iu", "ju"));
+        daTra = daTra.replaceAll("iu", "ju")
       }
-      //Controllo delle u
-      if(daTrascrivere.includes("ua") || daTrascrivere.includes("ue") || daTrascrivere.includes("uo") || daTrascrivere.includes("ui")){
-        daTrascrivere=daTrascrivere.replaceAll("u","w");
+      // Controllo delle U
+      if (daTra.includes("ua") || daTra.includes("ue") || daTra.includes("uo") || daTra.includes("ui")) {
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ua", "wa"));
+        daTra = daTra.replaceAll("ua", "wa")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ue", "we"));
+        daTra = daTra.replaceAll("ue", "we")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("uo", "wo"));
+        daTra = daTra.replaceAll("uo", "wo")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ui", "wi"));
+        daTra = daTra.replaceAll("ui", "wi")
+
+
       }
-      else
-      {
-        daTrascrivere=daTrascrivere.replaceAll("u","u");
+      // Controllo delle G
+      if (daTra.includes("ga") || daTra.includes("go") || daTra.includes("gu") || daTra.includes("gh")) {
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ga", "ga"));
+        daTra = daTra.replaceAll("ga", "ga")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("go", "go"));
+        daTra = daTra.replaceAll("go", "go")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("gu", "gu"));
+        daTra = daTra.replaceAll("gu", "gu")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("gh", "gh"));
+        daTra = daTra.replaceAll("gh", "gh")
       }
-      //Controllo delle g
-      if(daTrascrivere.includes("ga") || daTrascrivere.includes("go") || daTrascrivere.includes("gu") || daTrascrivere.includes("gh")){
-        daTrascrivere=daTrascrivere.replaceAll("g","g");
+      if (daTra.includes("g")) {
+        daTrascrivere = daTrascrivere.replace(daTra, daTra.replace(/g(?!([hauo]))/g, ''));
+        daTra = daTra.replace(/g(?!([hauo]))/g, '')
       }
-      else
-      {
-        daTrascrivere=daTrascrivere.replaceAll("g","");
+      //Controllo della Z
+      if (daTra.startsWith("z")) {
+        console.log("inizia con z: " + daTra);
+        daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("z", 'ʣ'));
+        daTra = daTra.replace("z", 'ʣ')
       }
-      //controllo delle c
-      if(daTrascrivere.includes("ca") || daTrascrivere.includes("co") || daTrascrivere.includes("cu") || daTrascrivere.includes("ch")){
-        daTrascrivere=daTrascrivere.replaceAll("c","k");
+      if (daTra.includes("z") && this.isZAtMiddle(daTra)) {
+        console.log("metà con z: " + daTra);
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("z", "ʦ:"));
+        daTra = daTra.replaceAll("z", "ʦ:")
       }
-      else
-      {
-        daTrascrivere=daTrascrivere.replaceAll("c","");
+      //Controllo della S
+      if (daTra.includes("s") || daTra.includes("sg") || daTra.includes("sb")
+        || daTra.includes("sd") || daTra.includes("sv") || daTra.includes("sm")
+        || daTra.includes("sl")) {
+          if(!daTra.startsWith("s")){
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sg", 'zg'));
+            daTra = daTra.replace("sg", 'zg')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sb", 'zb'));
+            daTra = daTra.replace("sb", 'zb')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sd", 'zd'));
+            daTra = daTra.replace("sd", 'zd')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sv", 'zv'));
+            daTra = daTra.replace("sv", 'zv')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sm", 'zm'));
+            daTra = daTra.replace("sm", 'zm')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace("sl", 'zl'));
+            daTra = daTra.replace("sl", 'zl')
+            daTrascrivere = daTrascrivere.replace(daTra, daTra.replace(/s(?![t|ca|co|cu|ch|p|r|f|:])/g, 'z'));
+            daTra = daTra.replace(/s(?![t|ca|co|cu|ch|p|r|f|:])/g, 'z')
+          }
+          else
+          {
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sg", 'zg'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sg", 'zg')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sb", 'zb'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sb", 'zb')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sd", 'zd'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sd", 'zd')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sv", 'zv'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sv", 'zv')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sm", 'zm'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sm", 'zm')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace("sl", 'zl'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace("sl", 'zl')
+            daTrascrivere = daTrascrivere.replace(daTra.substring(1, daTra.length), daTra.substring(1, daTra.length).replace(/s(?![t|ca|co|cu|ch|p|r|f|:])/g, 'z'));
+            daTra = "s"+daTra.substring(1, daTra.length).replace(/s(?![t|ca|co|cu|ch|p|r|f|:])/g, 'z')
+          }
       }
-      //Controllo della s
-      if(daTrascrivere.startsWith("s") || daTrascrivere.includes("s:") || 
-       daTrascrivere.includes("st") || daTrascrivere.includes("sca") || daTrascrivere.includes("sco")
-       || daTrascrivere.includes("scu") || daTrascrivere.includes("sch") || daTrascrivere.includes("sp")
-       || daTrascrivere.includes("sr") || daTrascrivere.includes("sf")){
-        daTrascrivere=daTrascrivere.replaceAll("s","s");
+      if (daTra.startsWith("s") || daTra.includes("s:") ||
+        daTra.includes("st") || daTra.includes("sca") || daTra.includes("sco")
+        || daTra.includes("scu") || daTra.includes("sch") || daTra.includes("sp")
+        || daTra.includes("sr") || daTra.includes("sf")) {
+        
       }
-      else if(daTrascrivere.includes("s") || daTrascrivere.includes("sg") || daTrascrivere.includes("sb")
-      || daTrascrivere.includes("sd") || daTrascrivere.includes("sv") || daTrascrivere.includes("sm")
-      || daTrascrivere.includes("sl")){
-        daTrascrivere=daTrascrivere.replaceAll("s","z");
+
+      //controllo delle C
+      if (daTra.includes("ca") || daTra.includes("co") || daTra.includes("cu") || daTra.includes("ch")) {
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ca", "ka"));
+        daTra = daTra.replaceAll("ca", "ka")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("co", "ko"));
+        daTra = daTra.replaceAll("co", "ko")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("cu", "ku"));
+        daTra = daTra.replaceAll("cu", "ku")
+        daTrascrivere = daTrascrivere.replaceAll(daTra, daTra.replaceAll("ch", "kh"));
+        daTra = daTra.replaceAll("ch", "kh")
+      }
+      if (daTra.includes("c")) {
+        daTrascrivere = daTrascrivere.replace(daTra, daTra.replace(/c(?!([hau]))/g, ''));
+        daTra = daTra.replace(/c(?!([hau]))/g, '')
       }
     }
 
-    for (let i = 0; i < this.scritta.length; i++) {
-      if(daTrascrivere.includes(this.scritta[i])){
-        daTrascrivere=daTrascrivere.replaceAll(this.scritta[i],this.fonemi[i]);
+    for (let i = 0; i < this.scrittaDopo.length; i++) {
+      if (daTrascrivere.includes(this.scrittaDopo[i])) {
+        daTrascrivere = daTrascrivere.replaceAll(this.scrittaDopo[i], this.fonemiDopo[i]);
       }
     }
-    this.trascritto=daTrascrivere;
+
+    this.trascritto = daTrascrivere;
+
+
   }
+  isZAtMiddle(word: any) {
+    var middleIndex = Math.floor(word.length / 2);
+    return word.charAt(middleIndex) === 'z';
+  }
+
 }
 
